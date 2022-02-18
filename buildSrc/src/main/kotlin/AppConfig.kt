17ead00634b1e513c6dc28ca3e5ae5336999dfbd
racka98 <bmkilaha@gmail.com>
2022-02-18 20:50:39 +0300

    version bump to beta

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,6 +2,6 @@ object AppConfig {
     const val compileSdkVersion = 31
     const val minSdkVersion = 23
     const val targetSdkVersion = 31
-    const val versionCode = 3
-    const val versionName = "1.0.0-alpha05"
+    const val versionCode = 4
+    const val versionName = "1.0.0-beta01"
 }
