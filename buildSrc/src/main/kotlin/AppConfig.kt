742651c9ae63dfc33f98ade07908692eb050abbd
racka98 <bmkilaha@gmail.com>
2022-03-01 16:19:47 +0300

    fixes

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -3,5 +3,5 @@ object AppConfig {
     const val minSdkVersion = 23
     const val targetSdkVersion = 31
     const val versionCode = 4
-    const val versionName = "1.0.0-beta01"
+    const val versionName = "1.0"
 }
